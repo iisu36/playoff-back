a80860e762f71d24cfa0b7b2ef803631575ca757
Iisakki Kulhua <iisakki@kulhua.net>
2021-04-22 15:10:34 +0300

    Pistepörssiin reegointi sijoituksissa, ja hieman väriä selkeydeksi

--- index.js
+++ index.js
@@ -12,6 +12,12 @@ app.use(express.static('build'))
 app.use(express.json())
 app.use(cors())
 
+app.use(function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "YOUR-DOMAIN.TLD"); // update to match the domain you will make the request from
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
+
 app.get('/anari', async (req, res) => {
     const result = await axios.get('https://statsapi.web.nhl.com/api/v1/standings')
 
@@ -74,6 +80,15 @@ app.get('/anari/pelaajat', (req, res) => {
     Pelaaja.find({}).then(pelaajat => {
         res.json(pelaajat)
     })
+})
+
+app.get('/anari/porssi', async (req, res) => {
+    const result = await axios.get('https://api.nhle.com/stats/rest/fi/leaders/skaters/points?cayenneExp=season=20202021%20and%20gameType=2')
+    const pelaaja = {
+        name: result.data.data[0].player.lastName,
+        points: result.data.data[0].points
+    }
+    res.json(pelaaja)
 })
 
 const PORT = process.env.PORT || 3001
