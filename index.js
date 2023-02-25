78a038e3d3064beaabcd56a1bb9aebffeb1af31d
Iisakki Kulhua <iisakki@kulhua.net>
2023-02-25 20:54:20 +0200

    mongoose update

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 require('dotenv').config()
 const express = require('express')
 const axios = require('axios')
+const mongoose = require('mongoose')
 const { response } = require('express')
 const cors = require('cors')
 
@@ -12,83 +13,99 @@ app.use(express.static('build'))
 app.use(express.json())
 app.use(cors())
 
-app.use(function (req, res, next) {
-    res.header("Access-Control-Allow-Origin", "YOUR-DOMAIN.TLD"); // update to match the domain you will make the request from
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    next();
-});
+/* app.use(function (req, res, next) {
+  res.header('Access-Control-Allow-Origin', 'YOUR-DOMAIN.TLD') // update to match the domain you will make the request from
+  res.header(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-Type, Accept'
+  )
+  next()
+}) */
+
+mongoose
+  .connect(process.env.MONGODB_URI)
+  .then(() => {
+    console.log('connected to MongoDB')
+  })
+  .catch((error) => {
+    console.log('error connection to MongoDB', error.message)
+  })
 
 app.get('/anari', async (req, res) => {
-    const result = await axios.get('https://statsapi.web.nhl.com/api/v1/standings')
+  const result = await axios.get(
+    'https://statsapi.web.nhl.com/api/v1/standings'
+  )
 
-    const allTeams = []
+  const allTeams = []
 
-    const divisions = result.data.records.map(division => {
+  const divisions = result.data.records.map((division) => {
+    const teams = division.teamRecords.map((team) => {
+      const teamToAdd = {
+        team: team.team.name,
+        teamId: team.team.id,
+        points: team.points,
+        divisionRank: team.divisionRank,
+        leagueRank: team.leagueRank,
+        lastUpdated: team.lastUpdated,
+      }
 
-        const teams = division.teamRecords.map(team => {
+      allTeams.push(teamToAdd)
 
-            const teamToAdd = {
-                team: team.team.name,
-                teamId: team.team.id,
-                points: team.points,
-                divisionRank: team.divisionRank,
-                leagueRank: team.leagueRank,
-                lastUpdated: team.lastUpdated
-            }
-
-            allTeams.push(teamToAdd)
-
-            return teamToAdd
-        })
-
-
-        return {
-            division: division.division.name,
-            teams: teams
-        }
+      return teamToAdd
     })
 
-    const stats = {
-        league: allTeams,
-        divisions: divisions
+    return {
+      division: division.division.name,
+      teams: teams,
     }
+  })
 
-    stats.league.sort((a, b) => a.leagueRank - b.leagueRank)
+  const stats = {
+    league: allTeams,
+    divisions: divisions,
+  }
 
-    res.json(stats)
+  stats.league.sort((a, b) => a.leagueRank - b.leagueRank)
+
+  res.json(stats)
 })
 
 app.post('/anari/pelaajat', (req, res, next) => {
-    const body = req.body
+  const body = req.body
 
-    const pelaaja = new Pelaaja({
-        standing: body.standing,
-        name: body.name,
-        teams: body.teams,
-        points: body.points,
-        pisteporssi: body.pisteporssi
+  const pelaaja = new Pelaaja({
+    standing: body.standing,
+    name: body.name,
+    teams: body.teams,
+    points: body.points,
+    pisteporssi: body.pisteporssi,
+  })
+
+  pelaaja
+    .save()
+    .then((savedPelaaja) => {
+      res.status(201).json(savedPelaaja.toJSON())
     })
-
-    pelaaja.save()
-        .then(savedPelaaja => {
-            res.status(201).json(savedPelaaja.toJSON())
-        })
-        .catch(error => next(error))
+    .catch((error) => next(error))
 })
 
-app.get('/anari/pelaajat', (req, res) => {
-    Pelaaja.find({}).then(pelaajat => {
-        res.json(pelaajat)
+app.get('/anari/pelaajat', async (req, res) => {
+  Pelaaja.find({})
+    .then((pelaajat) => {
+      res.json(pelaajat)
     })
+    .catch((error) => next(error))
 })
 
 app.get('/anari/porssi', async (req, res) => {
-    const result = await axios.get('https://api.nhle.com/stats/rest/fi/leaders/skaters/points?cayenneExp=season=20222023%20and%20gameType=2')
-    const pelaaja = {
-        name: result.data.data[0].player.lastName,
-        points: result.data.data[0].points
-    }
-    res.json(pelaaja)
+  const result = await axios.get(
+    'https://api.nhle.com/stats/rest/fi/leaders/skaters/points?cayenneExp=season=20222023%20and%20gameType=2'
+  )
+  const pelaaja = {
+    name: result.data.data[0].player.lastName,
+    points: result.data.data[0].points,
+  }
+  res.json(pelaaja)
 })
 
 const PORT = process.env.PORT || 3001
--- pelaaja.js
+++ pelaaja.js
@@ -1,37 +1,18 @@
 const mongoose = require('mongoose')
 
-const url = process.env.MONGODB_URI
-
-
-mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false, useCreateIndex: true })
-    .then(() => {
-        console.log('connected to MongoDB')
-    })
-    .catch((error) => {
-        console.log('error connecting to MongoDB:', error.message)
-    })
-
 const teamSchema = new mongoose.Schema({
-    name: String,
-    teamId: Number,
-    division: String,
-    points: Number
+  name: String,
+  teamId: Number,
+  division: String,
+  points: Number,
 })
 
 const pelaajaSchema = new mongoose.Schema({
-    standing: Number,
-    name: String,
-    teams: [teamSchema],
-    points: Number,
-    pisteporssi: Number
-})
-
-pelaajaSchema.set('toJSON', {
-    transform: (document, returnedObject) => {
-        returnedObject.id = returnedObject._id.toString()
-        delete returnedObject._id
-        delete returnedObject.__v
-    }
+  standing: Number,
+  name: String,
+  teams: [teamSchema],
+  points: Number,
+  pisteporssi: Number,
 })
 
 module.exports = mongoose.model('Pelaaja', pelaajaSchema)
